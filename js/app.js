d447b6284b629f6037ecea9ba719e3cf1e33c41c
Michael Falk <falk478@yahoo.com>
2020-10-05 14:02:42 -0700

    added css sheets, js sheets and HTML structure, started on first object

---
+++ js/app.js
@@ -0,0 +1,15 @@
+'use strict';
+//alert ('is this thing on? test 1, 2, 3,')
+//Object 1 naming seattleStore
+
+var seattleStore = {
+    name : 'Seattle',
+    minCustomer: 23,
+    maxCustomer: 65,
+    avgCookieSale: 6.3,
+    hoursOpen: 14,
+    calcNumberOfCustomers = function(){
+        //need random number generator here
+    } 
+    numbCustomersPerHour []
+};
