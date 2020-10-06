d78dd6f5cd4eb9d662deaf4021cbfe41ee3eaa4b
Michael Falk <falk478@yahoo.com>
2020-10-05 21:11:07 -0700

    this is not how to solve this but I'm out of ideas at this point

--- js/app.js
+++ js/app.js
@@ -1,15 +1,41 @@
 'use strict';
-//alert ('is this thing on? test 1, 2, 3,')
+
 //Object 1 naming seattleStore
 
 var seattleStore = {
-    name : 'Seattle',
+    name: 'Seattle',
+    hoursOpen: 14,
+    avgCookieSale: 6.3,
     minCustomer: 23,
     maxCustomer: 65,
-    avgCookieSale: 6.3,
-    hoursOpen: 14,
-    calcNumberOfCustomers = function(){
-        //need random number generator here
-    } 
-    numbCustomersPerHour []
-};
+    bananas: 
+    function storeVistors(){{for (var i = 0; i < 14; i++);{
+    function bananastwo(min, max)
+    return Math.floor(Math.random() * (max - min + 1))+ min;
+     }
+}
+    console.log (seattleStore.bananas(seattleStore.minCustomer, seattleStore.maxCustomer))
+
+
+
+//Global Functions
+// Customer Count Generator
+
+//for (var i = 0; i < 14; i++){
+
+//function getRandomCustomers(min,max){
+  //  min = Math.ceil(min);
+    //max = Math.floor(max);
+    //return Math.floor(Math.random()* (max-min+1)+min);
+//} 
+//CustomersPerHour.push(getRandomCustomers(seattleStore.minCustomer,seattleStore.maxCustomer))}
+
+
+// Customer Count Tracker Array
+
+//customersPerHour = []
+//console.log(customersPerHour[])
+
+
+
+   
