1b389d8fbde749f4cd5ca111c929f3a77dd639d7
Michael Falk <falk478@yahoo.com>
2020-10-07 19:16:34 -0700

    added footer row aand store totals by hour. The daily totals are still showing in the first row but are rendering and the total total still needs to be fixed

--- js/app.js
+++ js/app.js
@@ -1,23 +1,24 @@
 'use strict';
-//Global variable Store Hours
-var storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm','total'];
-
-//Object Constructor
-
+//Global variables
+var storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', 'total'];
+var parentElement = document.getElementById('table');
 var allStores = [];
 
-function Store(name, avgerageCookiePerCustomer, minimumCustomersPerHour, maximumCustomersPerHour, totalCookiesForTheDay = 0) {
+
+
+//Object Constructor Function
+function Store(name, avgerageCookiePerCustomer, minimumCustomersPerHour, maximumCustomersPerHour) {
   this.name = name;
   this.avgerageCookiePerCustomer = avgerageCookiePerCustomer;
   this.minimumCustomersPerHour = minimumCustomersPerHour;
   this.maximumCustomersPerHour = maximumCustomersPerHour;
-  this.totalCookiesForTheDay = totalCookiesForTheDay;
   this.customersEachHour = [];
   this.cookiesSoldEachHour = [];
+  this.totalCookiesForTheDay = 0;
   allStores.push(this);
 }
 Store.prototype.generateCustomersEachHour = function () {
-  for (var i = 0; i < storeHours.length-1; i++) {
+  for (var i = 0; i < storeHours.length - 1; i++) {
     var randomNumber = Math.floor(Math.random() * (this.maximumCustomersPerHour - this.minimumCustomersPerHour + 1) + this.minimumCustomersPerHour);
 
     this.customersEachHour.push(randomNumber);
@@ -25,7 +26,7 @@ Store.prototype.generateCustomersEachHour = function () {
 };
 
 Store.prototype.generateCookiesSoldEachHour = function () {
-  for (var i = 0; i < storeHours.length-1; i++) {
+  for (var i = 0; i < storeHours.length - 1; i++) {
     var cookiesSoldthisHour = Math.ceil(this.customersEachHour[i] * this.avgerageCookiePerCustomer);
 
     this.totalCookiesForTheDay += cookiesSoldthisHour;
@@ -33,6 +34,7 @@ Store.prototype.generateCookiesSoldEachHour = function () {
     this.cookiesSoldEachHour.push(cookiesSoldthisHour);
   }
 };
+
 
 //object instances to create stores
 var seattle = new Store('seattle', 6.3, 23, 65);
@@ -56,55 +58,84 @@ paris.generateCookiesSoldEachHour();
 lima.generateCustomersEachHour();
 lima.generateCookiesSoldEachHour();
 
-console.log(allStores);
-
-var parentElement = document.getElementById('table');
+//Create the header row
 function generateHeaderRow() {
-  //create a tr
   var trElement = document.createElement('tr');
-  var headerArray = storeHours;
-  for (var i = -1; i < headerArray.length; i++) {
-    //create a th
+  for (var i = -1; i < storeHours.length; i++) {
     var thElement = document.createElement('th');
-    // fill it with content - 'Name'
-    thElement.textContent = headerArray[i];
-    //append to the tr element
+    thElement.textContent = storeHours[i];
     trElement.appendChild(thElement);
   }
-  // append the tr to the tbody
   parentElement.appendChild(trElement);
 }
+//create footer row
+function generateFooterRow() {
+  var tfElement = document.createElement('tr');
+  var tfHeader = document.createElement('th');
+  tfHeader.textContent = 'Total';
+  tfElement.appendChild(tfHeader);
+  for (var i = -1; i < storeHours.length; i++) {
+    var eachHourTotal = 0;
 
-Store.prototype.renderData = function () {
-  //create a tr
-  var trElement = document.createElement('tr');
-  parentElement.appendChild(trElement);
-  var tdElement = document.createElement('td');
-  tdElement.textContent = this.name;
-  trElement.appendChild(tdElement);
+    for (var k = 0; k < allStores.length; k++) {
+      eachHourTotal += allStores[k].cookiesSoldEachHour[i];
+    }
+    var thElement4 = document.createElement('td');
+    thElement4.textContent = eachHourTotal;
+    tfElement.appendChild(thElement4);
+  }
+  parentElement.appendChild(tfElement);
+}
 
-  //var fluffyArray = this.cookiesSoldEachHour;
+// Populate the table with cookie sales results
+Store.prototype.render = function () {
+  var tableRowElement = document.createElement('tr');
+  var tableHeader = document.createElement('th');
+  tableHeader.textContent = this.name;
+  tableRowElement.appendChild(tableHeader);
+
+  var dailyCookieTotalsByStore = document.createElement('tr');
+  dailyCookieTotalsByStore.textContent = this.totalCookiesForTheDay;
+  tableRowElement.appendChild(dailyCookieTotalsByStore);
 
   for (var i = 0; i < storeHours.length; i++) {
-    //create a td
-    var td2Element = document.createElement('td');
-    // fill it with content
-    td2Element.textContent = this.cookiesSoldEachHour[i];
-    //append it to the tr
-    trElement.appendChild(td2Element);
 
-    var td3Element = document.createElement('td');
-    td3Element.textContent = this.totalCookiesForTheDay;
-    trElement.appendChild(td2Element);
-
-    //append the tr to the parent table
-    //trElement.appendChild(td2Element);
+    var totalCookiesData = document.createElement('td');
+    totalCookiesData.textContent = this.cookiesSoldEachHour[i];
+    tableRowElement.appendChild(totalCookiesData);
   }
+  parentElement.appendChild(tableRowElement);
 };
 
+// Store.prototype.renderTotals = function () {
+//   var tableRowElement3 = document.createElement('tr');
+//   var tableHeader3 = document.createElement('th');
+//   tableHeader3.textContent = 'total';
+//   tableRowElement3.appendChild(tableHeader3);
+
+//   for (var i = 0; i < allStores.length; i++) {
+
+//     var dailyCookieTotalsByStore = document.createElement('td');
+//     dailyCookieTotalsByStore.textContent = this.totalCookiesForTheDay[i];
+//     tableHeader3.appendChild(dailyCookieTotalsByStore);
+//   }
+//   parentElement.appendChild(tableRowElement3);
+// };
+
+// var td3Element = document.createElement('td');
+// td3Element.textContent = this.totalCookiesForTheDay;
+// td2Element.appendChild(td2Element);
+
+// //append the tr to the parent table
+// //trElement.appendChild(td2Element);
+
+
+
 generateHeaderRow();
-seattle.renderData();
-tokyo.renderData();
-dubai.renderData();
-paris.renderData();
-lima.renderData();
+seattle.render();
+tokyo.render();
+dubai.render();
+paris.render();
+lima.render();
+generateFooterRow();
+
